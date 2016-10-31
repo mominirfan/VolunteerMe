f3d5a11edd7ba9fe0a114382a5fa1e5c767582fd
leimajinguang <leimajinguang@sina.com>
2016-10-31 17:26:58 -0500

    mine
    mine

---
+++ app/postProject/postProject/postProject.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'postProject',
+	templateUrl: 'postProject.component.html'
+})
+
+export class PostProjectComponent implements OnInit {
+
+	ngOnInit() { }
+}
