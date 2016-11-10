115ffae7534e0ae3a0ec09c101a252ed8426a5f3
Rupal Sanghavi <rsanghavi@smu.edu>
2016-11-10 08:10:10 -0600

    temporarily commented out navbar

--- app/viewProject/viewProject.component.ts
+++ app/viewProject/viewProject.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'viewProject',
-	templateUrl: 'viewProject.component.html'
+	templateUrl: './app/viewProject/viewProject.component.html'
 })
 
 export class ViewProjectComponent implements OnInit {
