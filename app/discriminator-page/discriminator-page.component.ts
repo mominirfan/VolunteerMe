ca1da9114a4f97ee0ada4b3b58d46acd67f0f858
Calum Aldridge <omi462@yahoo.com>
2016-11-09 20:53:24 -0600

    added discrim page

---
+++ app/discriminator-page/discriminator-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
+@Component({
+  selector: 'discriminator-page',
+  templateUrl: './app/discriminator-page/discriminator-page.html',
+  styleUrls: [ './app/discriminator-page/discriminator-page.css' ]
+})
+
+export class DiscrimPageComponent {
+
+}
